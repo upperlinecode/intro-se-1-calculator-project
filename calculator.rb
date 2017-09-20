589610e3953c52da9249b50f211ea5b4ffd77b17
jolson615 <jolson615@gmail.com>
2017-09-20 12:26:06 -0500

    initial commit

---
+++ calculator.rb
@@ -0,0 +1,5 @@
+require_relative 'definitions.rb'
+# The line above will let you separate your concerns by defining methods your calculator might use in a separate file.
+
+puts "Welcome to the Ruby Calculator\n\nWhat would you like to do?"
+choice = gets.chomp
